2a4d2d0f2a0fb1919974e0741447bf6e5fc7b820
jalal ud din <jalalrashidi4@gamil.com>
2024-07-28 13:52:49 +0500

    - US-1: added pakages and dummy blog content and display them on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,46 @@
+//home page
 
+import axios from "axios";
+import Link from "next/link";
 
-export default function Home() {
+interface Blog {
+  id: number;
+  title: string;
+  summary: string;
+  content: string;
+  slug: string;
+}
+
+async function fetchBlogs(): Promise<Blog[]> {
+  try {
+    // Use axios to fetch data
+    const response = await axios.get<Blog[]>("http://localhost:3000/api/blogs");
+    return response.data; 
+  } catch (error) {
+    console.error("Error fetching blogs:", error);
+    throw new Error("Failed to fetch blogs");
+  }
+}
+
+export default async function Home() {
+  const blogs = await fetchBlogs();
+
   return (
-    <p>Blog</p>
+    <div>
+      <h1>Blog Posts</h1>
+      <div className="grid grid-cols-4 gap-4">
+      {blogs.length > 0 ? (
+        blogs.map((blog) => (
+          <div key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>{blog.summary}</p>
+            <Link className="" href={`/${blog.slug}`}>read more</Link>
+          </div>
+        ))
+      ) : (
+        <p>No blogs available</p>
+      )}
+      </div>
+    </div>
   );
 }
